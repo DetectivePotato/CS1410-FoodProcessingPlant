b65faa10281b73a1f00834aa3fbe044bd1c8970a
DetectivePotato <tj@wall5.co.uk>
2016-04-06 10:30:16 +0100

    Added files via upload

--- Simulator.java
+++ Simulator.java
@@ -1,20 +1,55 @@
-
+/**
+ * The simluator handles all actions in the system
+ * - Create all the machines in the plant
+ * - Generate food each step
+ * - Increment age and act on machines each step
+ * 
+ * @author Anthony Wall
+ *
+ */
 public class Simulator 
 {
-
+	private static int NUM_PACKERS = 2;
+	private static int PACK_TIME = 20;
+	private static int NUM_STEPS;
+	
 	public static void main(String[] args) 
 	{
-		ProcessingMachine cheeseProcessor = new ProcessingMachine(20, "Cheese", 1);
-		ProcessingMachine blueCheeseProcessor = new ProcessingMachine(20, "BlueCheese", 1);
-		ProcessingMachine soupProcessor = new ProcessingMachine(22, "SoupPowder", 1);
+		try
+		{
+			NUM_STEPS = Integer.parseInt(args[0]);
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
 		
-		PackingMachine packer1 = new PackingMachine(10);
-		PackingMachine packer2 = new PackingMachine(10);
-		
-		System.out.println("Total Machines: " + (MachineList.getNumberofPackers() + MachineList.getNumberofProcessors()) );
-		System.out.println("    Total Packers:    " + MachineList.getNumberofPackers());
-		System.out.println("    Total Processors: " + MachineList.getNumberofProcessors());
+		setup();
 
+	}
+	/**
+	 * Creates all the machines required in the system
+	 */
+	private static void setup()
+	{
+		MachineFactory.generateMachines(PACK_TIME, NUM_PACKERS);
+		
+		System.out.println(MachineList.printDetails());
+	}
+	
+	/**
+	 * Perform all the required actions for simulation over a single step
+	 */
+	private static void oneStep()
+	{
+		//Create a new food object randomly
+		FoodFactory.generate();
+		
+		//Increment the age of all food and perform spoil checking
+		FoodList.ageFood();
+		
+		//Tell all machines to act
+		MachineList.activateMachines();
 	}
 
 }
