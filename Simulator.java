2b452548ec2e80e49d50016c3b6fa1fd40aa6b62
steveforrest <steveforrest_1@hotmail.com>
2016-04-18 13:38:19 +0100

    Added files via upload

--- Simulator.java
+++ Simulator.java
@@ -1,91 +1,55 @@
-import java.lang.StringBuilder;
-
-public class Simulator {
-
-	private static int numberOfPackers;
-	private static int packingTime = 10;
-	private static int num_Steps;
-	private double p;
-	private double q;
-
-
-	public void stop(){
-		int b = 0;
-		if(b > num_Steps)
+/**
+ * The simluator handles all actions in the system
+ * - Create all the machines in the plant
+ * - Generate food each step
+ * - Increment age and act on machines each step
+ * 
+ * @author Anthony Wall
+ *
+ */
+public class Simulator 
+{
+	private static int NUM_PACKERS = 2;
+	private static int PACK_TIME = 20;
+	private static int NUM_STEPS;
+	
+	public static void main(String[] args) 
+	{
+		try
 		{
-
-			//code to kill simulation
-			b++;
+			NUM_STEPS = Integer.parseInt(args[0]);
 		}
-		else
-			setup();
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+		
+		setup();
 
 	}
-	private void setup()
+	/**
+	 * Creates all the machines required in the system
+	 */
+	private static void setup()
 	{
-		MachineFactory.generateMachines(packingTime, numberOfPackers);
-
+		MachineFactory.generateMachines(PACK_TIME, NUM_PACKERS);
+		
 		System.out.println(MachineList.printDetails());
 	}
-
-	private static void singleStep()
+	
+	/**
+	 * Perform all the required actions for simulation over a single step
+	 */
+	private static void oneStep()
 	{
+		//Increment the age of all food and perform spoil checking
 		FoodList.ageFood();
-
+		
+		//Create a new food object randomly
 		FoodFactory.generate();
-
+		
+		//Tell all machines to act
 		MachineList.activateMachines();
-	}
-	public void setPProbs(int pProbs)
-	{
-
-		p = pProbs;
-
-	}
-
-	public void setQProbs(int qProbs)
-	{
-
-		q = qProbs;
-
-	}
-
-	public void setProcessingStages(int newStages){
-
-		ProcessingMachine.setProcessingStages(newStages);
-
-	}
-	public void setNumberOfPackers(int newPackerNum)
-	{
-		numberOfPackers = newPackerNum;
-	}
-	private void StringBuilder(String sb) {
-
-		StringBuilder s1 = new StringBuilder("Items Lost: " + ProfitStats.LOST_ITEMS);
-		StringBuilder s2 = new StringBuilder("Items Packed: " + ProfitStats.PACKED_ITEMS);
-		System.out.println(s1 + "/n" + s2);
-	}
-
-	public void probsValue(){
-
-		StringBuilder str =new StringBuilder("new P Value" + p);
-		StringBuilder str1 = new StringBuilder("new Q value" + q);
-
-		System.out.println(str + "/n" + str1);
-	}
-
-
-
-
-	static void Run()
-	{
-		while(num_Steps < 7200)
-		{
-
-			singleStep();
-			num_Steps++;
-		}
-
 	}
 
 }
