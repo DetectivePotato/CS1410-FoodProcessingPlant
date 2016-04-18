a95b6387cfb1d1d565d3b3fcf089d1d94225ba03
DetectivePotato <tj@wall5.co.uk>
2016-04-18 14:26:03 +0100

    Added files via upload

--- FoodFactory.java
+++ FoodFactory.java
@@ -1,34 +1,51 @@
-
-
 import java.util.Random;
-
+/**
+ * FoodFactory handles the random generation of food at each step
+ * and assigning it to the correct machines in the system
+ * 
+ * @author Steve Forrest
+ * @author Anthony Wall
+ *
+ */
 public class FoodFactory {
 
-	private static double Q_PROBABILITY = 0.1;
-	private static double P_PROBABILITY = 0.1;
-	private static int SEED = 42;
+	private static double Q_PROBABILITY = 0.05;
+	private static double P_PROBABILITY = 0.02;
+	private static int SEED = 999999999;
 	private static Random rnd = new Random(SEED);
 
+	/**
+	 * Generates Food objects based on the random number generator
+	 * and puts them in the correct processing machine
+	 * 
+	 * @return
+	 */
 	public static String generate()
 	{ 
+		//Generate the random number
 		double val = rnd.nextDouble();
+		
+		//Check if a non-perishable object is to be made
 		if(val <= Q_PROBABILITY)
 		{
-			//make item
-			//return new SoupPowder();
+			ProcessingMachine processor = MachineList.getNextProcessor("SoupPowder", 0);
 			
-			return "Made Non-Perishable";
+			processor.queueItem(new SoupPowder(processor));
+			System.out.println("Made Non-Perishable");
 		}
 		
 		if(val <= P_PROBABILITY)
 		{
-			//Make item
-			//return new Cheese();
-			return "Made Non-Perishable";
+			ProcessingMachine processorCheese = MachineList.getNextProcessor("Cheese", 0);
+			ProcessingMachine processorBCheese = MachineList.getNextProcessor("BlueCheese", 0);
+			
+			processorCheese.queueItem(new BlueCheese(processorCheese));
+			processorBCheese.queueItem(new BlueCheese(processorBCheese));
+			
+			System.out.println("Made Perishables");
 		}
 		
 		return "" + val;
-
 	}
 
 	public static void setQ(double newVal)
